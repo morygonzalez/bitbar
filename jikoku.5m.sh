451d9e7fda3fa2e3f3ecebd2e518421ede018023
森井ゴンザレス <morygonzalez@gmail.com>
2016-10-30 22:07:33 +0900

    Update jikoku path

--- jikoku.5m.sh
+++ jikoku.5m.sh
@@ -30,7 +30,7 @@ url="${url}&kind=${kind}"
 curl -s "${url}" |\
     /usr/local/bin/pup 'table.tblDiaDetail [id*="hh_"] json{}' |\
     /usr/local/bin/jq '[.[] | { hour: .children[0].text, minutes: [.children[1].children[].children[].children[].children[].children | map(.text) | join(" ") ] }]' |\
-    /Users/morygonzalez/src/github.com/morygonzalez/commute-timer/jikoku -f "${filter}"
+    /Users/morygonzalez/src/github.com/morygonzalez/jikoku/jikoku -f "${filter}"
 
 echo "---"
 echo "Refresh | refresh=true"
